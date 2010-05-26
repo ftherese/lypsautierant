444b10f0b733f6ef62748050e70bb3f86488ada2
br.ftherese@gmail.com <br.ftherese@gmail.com@61795ffd-28aa-6617-e6ef-811111c0bfc2>
2010-05-26 02:23:16 +0000

    first working alternating mode endings, with mode directory to make your own modules.

---
+++ psautier/modes/one.pm
@@ -0,0 +1,155 @@
+package modes::one;
+
+sub first {
+ my $c = 1;
+ my @a;
+ my @l = split(' ',shift); 
+ while (@l){
+  my $syl = pop @l;
+  if ($syl ne '--'){
+   if (($c == 2)||($c == 6)){
+    push(@a,'\\pl{'.$syl.'}');
+   }
+   elsif(($c == 3)||($c == 5)){
+    push(@a,'\\mi{'.$syl.'}');
+   }
+   else{
+    push(@a,$syl);
+   }
+   $c++;
+  }
+  else{
+   push(@a,$syl);
+  }
+ }
+ my $r = join(" ",reverse @a);
+ $r =~ s/ -- //g;
+ return $r;
+}
+
+sub flex {
+ my $c = 1;
+ my @a;
+ my @l = split(' ',shift); 
+ while (@l){
+  my $syl = pop @l;
+  if ($syl ne '--'){
+   if ($c == 2){
+    push(@a,'\\mi{'.$syl.'}');
+   }
+   else{
+    push(@a,$syl);
+   }
+   $c++;
+  }
+  else{
+   push(@a,$syl);
+  }
+ }
+ my $r = join(" ",reverse @a);
+ $r =~ s/ -- //g;
+ return $r;
+}
+
+sub a {
+ my $c = 1;
+ my @a;
+ my @l = split(' ',shift); 
+ while (@l){
+  my $syl = pop @l;
+  if ($syl ne '--'){
+   if (($c == 2)||($c == 3)){
+    push(@a,'\\pl{'.$syl.'}');
+   }
+   elsif(($c == 4)||($c == 5)){
+    push(@a,'\\mi{'.$syl.'}');
+   }
+   else{
+    push(@a,$syl);
+   }
+   $c++;
+  }
+  else{
+   push(@a,$syl);
+  }
+ }
+ my $r = join(" ",reverse @a);
+ $r =~ s/ -- //g;
+ return $r;
+}
+
+sub b {
+ my $c = 1;
+ my @a;
+ my @l = split(' ',shift); 
+ while (@l){
+  my $syl = pop @l;
+  if ($syl ne '--'){
+   if (($c == 1)||($c == 2)||($c == 4)){
+    push(@a,'\\mi{'.$syl.'}');
+   }
+   else{
+    push(@a,$syl);
+   }
+   $c++;
+  }
+  else{
+   push(@a,$syl);
+  }
+ }
+ my $r = join(" ",reverse @a);
+ $r =~ s/ -- //g;
+ return $r;
+}
+
+sub a_prime {
+ my $c = 1;
+ my @a;
+ my @l = split(' ',shift); 
+ while (@l){
+  my $syl = pop @l;
+  if ($syl ne '--'){
+   if ($c == 2){
+    push(@a,'\\pl{'.$syl.'}');
+   }
+   elsif(($c == 4)||($c == 1)){
+    push(@a,'\\mi{'.$syl.'}');
+   }
+   else{
+    push(@a,$syl);
+   }
+   $c++;
+  }
+  else{
+   push(@a,$syl);
+  }
+ }
+ my $r = join(" ",reverse @a);
+ $r =~ s/ -- //g;
+ return $r;
+}
+
+sub b_prime {
+ my $c = 1;
+ my @a;
+ my @l = split(' ',shift); 
+ while (@l){
+  my $syl = pop @l;
+  if ($syl ne '--'){
+   if(($c == 1)||($c == 2)||($c == 3)||($c == 5)){
+    push(@a,'\\mi{'.$syl.'}');
+   }
+   else{
+    push(@a,$syl);
+   }
+   $c++;
+  }
+  else{
+   push(@a,$syl);
+  }
+ }
+ my $r = join(" ",reverse @a);
+ $r =~ s/ -- //g;
+ return $r;
+}
+return 1;
