9e721ec0a1abb3432358501053b48efe22ce9e11
ftherese <br_franics_therese@yahoo.com>
2019-04-10 18:44:44 -0500

    Updated Psalter Creation Scripts

--- psautier/gregorian/one.pm
+++ psautier/gregorian/one.pm
@@ -4,12 +4,17 @@ sub first {
  my $ac = 0;
  my $sc = 0;
  my $ec = 0;
+ my $idx = 0;
+ my $mi = 0;
  my @a;
+ my @i;
  my @l = split(' ',shift); 
  while (@l){
   my $syl = pop @l;
+  $idx++;
   if ($syl ne '--'){
    $sc++;
+   push (@i, $idx - 1);
    if ($syl=~tr/áéíóúýÁÉÍÓÚÝ//){
     $ac++;
     if (($ac == 1)&&($sc == 1)){
@@ -17,6 +22,9 @@ sub first {
     }
     elsif($ac == 1){
      push(@a, '\\mi{'.$syl.'}');
+     $mi++;
+     my $csyl = $a[$i[$sc-2]];
+     $a[$i[$sc-2]] = '\\pl{'.$csyl.'}';
     }
     elsif(($ac == 2)&&($sc == 3)){
      push(@a,'\\mi{'.$syl.'}');
@@ -24,9 +32,17 @@ sub first {
     }
     elsif(($ac == 2)&&($sc !=  2)){
      push(@a,'\\pl{'.$syl.'}');
+     my $csyl = $a[$i[$sc-2]];
+     $a[$i[$sc-2]] = '\\mi{'.$csyl.'}';
+     if ($mi == 0){
+      $csyl = $a[$i[$sc-3]];
+      $a[$i[$sc-3]] = '\\mi{'.$csyl.'}';
+     }
     }
     elsif(($ac == 3)&&($ec == 1)){
      push(@a,'\\pl{'.$syl.'}');
+     my $csyl = $a[$i[$sc-2]];
+     $a[$i[$sc-2]] = '\\mi{'.$csyl.'}';
     }
     else{
      push(@a,$syl);
@@ -83,12 +99,16 @@ sub a {
  my $sc = 0;
  my $ac = 0;
  my $mc = 0;
+ my $idx = -1;
+ my @i;
  my @a;
  my @l = split(' ',shift); 
  while (@l){
   my $syl = pop @l;
+  $idx++;
   if ($syl ne '--'){
    $sc++;
+   push(@i, $idx);
    if (($syl=~tr/áéíóúýÁÉÍÓÚÝ//)&&($ac == 0)){
     $ac++;
     if ($sc == 1){
@@ -96,6 +116,8 @@ sub a {
     }
     else {
      push(@a,'\\pl{'.$syl.'}');
+     my $csyl = $a[@i[$sc-2]];
+     $a[@i[$sc-2]] = '\\pl{'.$csyl.'}';
     }
     $mc++;
    }
@@ -120,15 +142,23 @@ sub d {
  my $sc = 0;
  my $ac = 0;
  my $mc = 0;
+ my $idx = -1;
+ my @i;
  my @a;
  my @l = split(' ',shift); 
  while (@l){
   my $syl = pop @l;
+  $idx++;
   if ($syl ne '--'){
    $sc++;
+   push(@i, $idx);
    if (($syl=~tr/áéíóúýÁÉÍÓÚÝ//)&&($ac == 0)){
     $ac++;
-    push(@a,'\\pl{'.$syl.'}');
+    push(@a,'\\plmi{'.$syl.'}');
+    if ($sc != 1){
+     my $csyl = $a[$i[$sc -2]];
+     $a[$i[$sc -2]] = '\\mi{'.$csyl.'}';
+    }
     $mc++;
    }
    elsif (($mc == 1 )||($mc == 2)){
@@ -138,57 +168,6 @@ sub d {
    else{
     push(@a,$syl);
    }
-  }
-  else{
-   push(@a,$syl);
-  }
- }
- my $r = join(" ",reverse @a);
- $r =~ s/ -- //g;
- return $r;
-}
-
-sub a_prime {
- my $c = 1;
- my @a;
- my @l = split(' ',shift); 
- while (@l){
-  my $syl = pop @l;
-  if ($syl ne '--'){
-   if ($c == 2){
-    push(@a,'\\pl{'.$syl.'}');
-   }
-   elsif(($c == 4)||($c == 1)){
-    push(@a,'\\mi{'.$syl.'}');
-   }
-   else{
-    push(@a,$syl);
-   }
-   $c++;
-  }
-  else{
-   push(@a,$syl);
-  }
- }
- my $r = join(" ",reverse @a);
- $r =~ s/ -- //g;
- return $r;
-}
-
-sub b_prime {
- my $c = 1;
- my @a;
- my @l = split(' ',shift); 
- while (@l){
-  my $syl = pop @l;
-  if ($syl ne '--'){
-   if(($c == 1)||($c == 2)||($c == 3)||($c == 5)){
-    push(@a,'\\mi{'.$syl.'}');
-   }
-   else{
-    push(@a,$syl);
-   }
-   $c++;
   }
   else{
    push(@a,$syl);
