14dabfbd27c20ee8c62497e1a50e9d0bdcfdd7c4
ftherese <br_franics_therese@yahoo.com>
2019-05-11 21:40:32 -0600

    Updated the build command.

--- psautier/sampleBuildTheBookCommand.sh
+++ psautier/sampleBuildTheBookCommand.sh
@@ -1,9 +1,9 @@
 export PSALTERDIR=$PWD;
-lilypond-book -I$PWD/../en-US/  -I./ --latex-program=xelatex --pdf newPsalter.tex --output test;
-#lilypond-book -I../  -I./ --pdf newPsalter1-49.tex --output test;
-#lilypond-book -I../  -I./ --pdf newPsalter50-99.tex --output test;
-cd test;
-xelatex -output-driver="xdvipdfmx -q -E" --interaction=nonstopmode newPsalter.tex;
+lilypond-book -I$PWD/../en-US/  -I./ --latex-program=xelatex --pdf $1.tex --output $2;
+#lilypond-book -I../  -I./ --pdf newPsalter1-49.tex --output $2;
+#lilypond-book -I../  -I./ --pdf newPsalter50-99.tex --output $2;
+cd $2;
+xelatex -output-driver="xdvipdfmx -q -E" --interaction=nonstopmode $1.tex;
 #pdflatex newPsalter1-49.tex;
 #pdflatex newPsalter50-99.tex;
 cd ..;
