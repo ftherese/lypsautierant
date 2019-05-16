528b304aeca38bce91be42260845ed32cf705233
ftherese <br_franics_therese@yahoo.com>
2019-05-15 23:48:48 -0600

    added files

--- psautier/sampleBuildTheBookCommand.sh
+++ psautier/sampleBuildTheBookCommand.sh
@@ -1,7 +1,7 @@
 export PSALTERDIR=$PWD;
 lilypond-book -I$PWD/../en-US/  -I./ --latex-program=xelatex --pdf $1.tex --output $2;
-#lilypond-book -I../  -I./ --pdf newPsalter1-49.tex --output $2;
-#lilypond-book -I../  -I./ --pdf newPsalter50-99.tex --output $2;
+#lilypond-book -I../  -I./ --pdf newPsalter1-49.tex --output test;
+#lilypond-book -I../  -I./ --pdf newPsalter50-99.tex --output test;
 cd $2;
 xelatex -output-driver="xdvipdfmx -q -E" --interaction=nonstopmode $1.tex;
 #pdflatex newPsalter1-49.tex;
